fa1158c5f4a931674276f304031b93b1c4040703
Romain <fevre.romain@gmail.com>
2018-06-25 09:25:14 +0200

    Initial commits

---
+++ config.js
@@ -0,0 +1,54 @@
+// --------------------------------------------------------
+//
+// This file is to configure the configurable settings.
+// Load this file before script.js file at gmap.html.
+//
+// --------------------------------------------------------
+
+// -- Output Settings -------------------------------------
+// Show metric values
+// This controls the units used in the plane table,
+// and whether metric or imperial units are shown first
+// in the detailed plane info.
+Metric = false; // true or false
+
+// -- Map settings ----------------------------------------
+// These settings are overridden by any position information
+// provided by dump1090 itself. All positions are in decimal
+// degrees.
+
+// Default center of the map.
+DefaultCenterLat = 46.253917;
+DefaultCenterLon = 6.132222;
+// The google maps zoom level, 0 - 16, lower is further out
+DefaultZoomLvl   = 9;
+
+SiteShow    = true;           // true to show distances from site
+SiteDisplay = false;           // true to display site location
+SiteLat     = 46.332040;            // position of the marker
+SiteLon     = 6.057147;
+SiteName    = "My Radar Site"; // tooltip of the marker
+
+
+// -- Marker settings -------------------------------------
+// The default marker color
+MarkerColor	  = "rgb(221, 221, 221)";
+SelectedColor = "rgb(215, 223, 86)";
+StaleColor = "rgb(119, 119, 119)";
+
+
+SiteCircles = false; // true to show circles (only shown if the center marker is shown)
+// In nautical miles or km (depending settings value 'Metric')
+SiteCirclesDistances = new Array(50,100,150,200);
+
+// Show the clocks at the top of the righthand pane? You can disable the clocks if you want here
+ShowClocks = true;
+
+// Controls page title, righthand pane when nothing is selected
+PageName = "RMF - 1090MHz Radar";
+
+//Type of aircraft icon
+ShowAircraftIcon = true;
+
+// Runway in use
+RunwayInUse = 23;
---
+++ extension.js
@@ -0,0 +1,45 @@
+// -----------------------------------------------------
+//
+// This file is so users can modify how the page acts
+// without diving to deep in the code.  This way we can
+// also try out or hold custom code for ourselves and
+// not check it into the repo.
+//
+// There is a div id'ed as plane_extension for use with
+// this javascript file.
+// -----------------------------------------------------
+
+function extendedInitialize() {
+	// Write your initalization here
+	// Gets called just before the 1-sec function call loop is setup
+	updateClock();
+	updateRunway();
+	loadMapData();
+}
+
+function extendedPulse() {
+	// This will get called every second after all the main functions
+	updateClock();
+}
+
+function updateClock() {
+	var date = new Date();
+	var hours = date.getUTCHours().toString().length < 2 ? "0"+date.getUTCHours() : date.getUTCHours();
+	var minutes = date.getUTCMinutes().toString().length < 2 ? "0"+date.getUTCMinutes() : date.getUTCMinutes();
+	var seconds = date.getUTCSeconds().toString().length < 2 ? "0"+date.getUTCSeconds() : date.getUTCSeconds();
+	document.getElementById("utcClock").innerHTML = hours + ":" + minutes + ":" + seconds + " UTC";
+}
+
+function updateRunway() {
+	var runwayNumber = RunwayInUse < 10 ? '0' + RunwayInUse : RunwayInUse;
+	document.getElementById("runway").innerHTML = 'Displayed configuration : <span style="color: #caebee;">Runway ' + runwayNumber + '</span>';
+}
+
+function loadMapData() {
+	registerNavaidsFixes();
+	registerProcedures();
+	
+	loadAirspaces();
+	loadGeography();
+	loadNavdata();
+}
